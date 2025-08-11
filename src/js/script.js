f13e1d58bed92bd331b84d529e929e90e5ea9cea
steffDev144 <steffdevoff@hotmail.com>
2025-08-11 22:33:04 +0500

    start submenu in header

--- src/js/script.js
+++ src/js/script.js
@@ -94,14 +94,6 @@ document.addEventListener("DOMContentLoaded", () => {
 				toggleDropdown(false);
 			}
 		});
-
-		document.addEventListener("click", (event) => {
-			const isOutsideClick = !customSelect.contains(event.target);
-
-			if (isOutsideClick) {
-				toggleDropdown(false);
-			}
-		});
 	});
 
 	// укоротитель текста 3000
@@ -115,4 +107,29 @@ document.addEventListener("DOMContentLoaded", () => {
 	document.querySelectorAll('.blog__list_item-descr p').forEach(item => {
 		item.textContent = truncateString(item.textContent, 293);
 	})
+
+	// Под меню под меню того меню 3000
+	document.querySelectorAll('.header__item.sub').forEach(item => {
+		item.addEventListener('click', (e) => {
+			e.preventDefault();
+
+		})
+	})
+	document.querySelectorAll('.header__item_bool').forEach(item => {
+		const kill = item.querySelector('.header__item_bool-switch');
+		let bool = false;
+
+		kill.addEventListener('click', () => {
+			item.setAttribute('data-tab', `${!bool}`)
+			bool = !bool;
+			item.parentElement.querySelectorAll('.header__item_list').forEach(list => {
+				if(list.getAttribute('data-tab') !== `${!bool}`) {
+					list.classList.add('active');
+				} else {
+					list.classList.remove('active');
+				}
+			})
+		})
+	})
+
 });
