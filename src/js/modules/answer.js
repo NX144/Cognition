5d18ceb32e248d046dca3709943254b9e846f21a
steffDev144 <steffdevoff@hotmail.com>
2025-08-23 01:57:27 +0500

    zaza kpt adaptation complited

--- src/js/modules/answer.js
+++ src/js/modules/answer.js
@@ -5,7 +5,7 @@ export default function answer() {
             e.preventDefault();
             item.classList.toggle("active");
             if(item.classList.contains("active")) {
-                item.style.height = item.querySelector(".answer__wrap").offsetHeight + item.querySelector("a").offsetHeight + "px";
+                item.style.height = item.querySelector(".answer__wrap").offsetHeight + item.querySelector("a").offsetHeight - 1 + "px";
             } else {
                 item.style.height = item.querySelector("a").offsetHeight + "px";    
             }
